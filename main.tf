36ee41b8058a902f5baa435758cc9ce3b1b03745
Daniel Kay <daniel.kay@jet2.com>
2020-06-30 12:57:27 +0100

    added missing iam role

--- main.tf
+++ main.tf
@@ -3,6 +3,25 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_iam_role" "example" {
+  name = "example"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "appsync.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy" "example" {
   name = "example"
   role = "${aws_iam_role.example.id}"
