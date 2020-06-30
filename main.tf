8c2b49966973950ae5709c4a3c78c55a94e7a7ec
Daniel Kay <daniel.kay@jet2.com>
2020-06-30 13:35:55 +0100

    schema stuff

--- main.tf
+++ main.tf
@@ -48,37 +48,37 @@ resource "aws_appsync_graphql_api" "example" {
   authentication_type = "API_KEY"
   name                = "tf_appsync_example"
   schema              = <<EOF
-    type Flight {
-        id: ID!
-        departureDate: String!
-        departureAirportCode: String!
-        departureAirportName: String!
-        departureCity: String!
-        departureLocale: String!
-        arrivalDate: String!
-        arrivalAirportCode: String!
-        arrivalAirportName: String!
-        arrivalCity: String!
-        arrivalLocale: String!
-        ticketPrice: Int!
-        ticketCurrency: String!
-        flightNumber: Int!
-        seatAllocation: Int # Disabling due to regression in amplify-cli 4.13.1: @deprecated(reason: "use seatCapacity instead. seatAllocation will be removed in the stable release.")
-        seatCapacity: Int!
-    }
+type Flight {
+    id: ID!
+    departureDate: String!
+    departureAirportCode: String!
+    departureAirportName: String!
+    departureCity: String!
+    departureLocale: String!
+    arrivalDate: String!
+    arrivalAirportCode: String!
+    arrivalAirportName: String!
+    arrivalCity: String!
+    arrivalLocale: String!
+    ticketPrice: Int!
+    ticketCurrency: String!
+    flightNumber: Int!
+    seatAllocation: Int # Disabling due to regression in amplify-cli 4.13.1: @deprecated(reason: "use seatCapacity instead. seatAllocation will be removed in the stable release.")
+    seatCapacity: Int!
+}
 
-    type ModelFlightConnection {
-        items: [Flight]
-    }
+type ModelFlightConnection {
+    items: [Flight]
+}
 
-    type Query {
-        listFlights() : ModelFlightConnection
-    }
+type Query {
+    listFlights() : ModelFlightConnection
+}
 
-    schema {
-        query: Query
-    }
-    EOF
+schema {
+    query: Query
+}
+EOF
 }
 
 resource "aws_appsync_datasource" "example" {
